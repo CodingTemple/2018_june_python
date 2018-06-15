76205298a08096a5a6e170902fc3d54ae6fe1901
derekmhawkins <derekmhawkins@gmail.com>
2018-06-15 09:39:15 -0500

    added classwork from 06-14

---
+++ week2/06-14/modules.py
@@ -0,0 +1,8 @@
+def greeting(name):
+  print(f"Hello, {name}")
+
+icecream = {
+    "flavor": "Cookie Dough"
+}
+
+var1 = "Tank"
---
+++ week2/06-14/modules2.py
@@ -0,0 +1,11 @@
+dict1 = {
+    "name": "Derek",
+    "age": 30,
+    "sex": "M"
+}
+
+dict2 = {
+    "name": "Zara",
+    "age": 7,
+    "sex": "F"
+}
